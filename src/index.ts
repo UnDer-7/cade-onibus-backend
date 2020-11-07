35a294f254f8de48037de92ad02f05265c510987
UnDer-7 <mateus7532@gmail.com>
2020-11-07 20:50:03 -0300

    added more log info

--- src/index.ts
+++ src/index.ts
@@ -12,5 +12,7 @@ server.express.listen(port, () => {
   console.log(`ENVIRONMENT:     ${process.env.NODE_ENV}`);
   console.log(`SERVER VERSION:  ${process.env.npm_package_version}`);
   console.log(`Allowed origins: ${server.origins.join(' | ')}`);
+  console.log(`Database URL:    ${process.env.DB_URL}`);
+  console.log(`Database Name:   ${process.env.DB_NAME}`);
   console.log('----------------------\n');
 });
